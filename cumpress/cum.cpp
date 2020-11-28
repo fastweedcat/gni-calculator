581ceb360c181a7b6e9af156e871d441a367dfb8
me <75113920+fastweedcat@users.noreply.github.com>
2020-11-28 02:49:40 +0000

    Add files via upload

---
+++ cumpress/cum.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+  int gni;
+  int gni2;
+
+  string bruh;
+
+  cout<<"/,+,-,X and the numbers u want to get fucked lo\n";
+
+  cin >> bruh;
+
+  cin >> gni;
+  cin >> gni2;
+
+  if (bruh == "-"){
+      cout << gni-gni2;
+    }
+
+  if (bruh == "+"){
+      cout << gni+gni2;
+    }
+
+  if (bruh == "/"){
+      cout << gni/gni2;
+    }
+
+  if (bruh == "x"){
+      cout << gni*gni2;
+  }
+
+  cout << "\n";
+
+  system("pause");
+
+  
+
+
+}
